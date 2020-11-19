1d877e9d11073cfdc428ccd6cf528c626254f743
erirapce <hermesrapce@gmail.com>
2020-11-19 10:53:09 +0100

    Upload Files

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rb.useGravity = false;  
+    }
+
+    // Update is called once per frame 
+    // But we marked it as FixedUpdate because
+    // we are using physics in it.
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, 1000 * Time.deltaTime);
+    }
+}
